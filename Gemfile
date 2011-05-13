d92655f09487a62a8bbfee13c62932a54ec5c62c
Warren Li <warrenli@dontrush.org>
2011-05-13 12:09:17 +0800

    Creation of projects feature complete

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'dynamic_form'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.4'
+  gem 'fuubar'
+end
+
+group :test do
+  gem 'autotest'
+  gem 'email_spec'
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'launchy'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'bcdaea2b081da6e6fd7db361660c30e2fc7c8dceb57f81a978f56681f775e029055bfd24ddd12782b063807a596fe501dbae1541037cd921a8fcbcbd50185033'
