70eb61ea82f8fe319c50a9c8a23ca26315b65658
Warren Li <warrenli@dontrush.org>
2011-05-17 16:15:56 +0800

    Display a comment's state transition

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,13 @@
 module TicketsHelper
+  def state_for(comment)
+    content_tag(:div, :class => "states") do
+      if comment.state
+        if comment.previous_state && comment.state != comment.previous_state
+          "#{State.model_name.human}: #{render comment.previous_state} &rarr; #{render comment.state}".html_safe
+        else
+          "#{State.model_name.human}: #{render comment.state}".html_safe
+        end
+      end
+    end
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ActiveRecord::Base
   after_create :set_ticket_state
+  before_create :set_previous_state
   belongs_to :ticket
   belongs_to :user
   belongs_to :state
+  belongs_to :previous_state, :class_name => "State"
   validates_presence_of :text
 
   private
@@ -10,5 +12,9 @@ class Comment < ActiveRecord::Base
   def set_ticket_state
     self.ticket.state = self.state
     self.ticket.save!
+  end
+
+  def set_previous_state
+    self.previous_state = ticket.state
   end
 end
