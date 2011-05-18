db53208289f94840fb37ec492bd7a12ded0ee465
Warren Li <warrenli@dontrush.org>
2011-05-18 11:37:38 +0800

    Protect state_id from users who do not have permission to change it

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,57 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
 describe CommentsController do
 
+  let(:project) { Project.create!(:name => "Ticketee") }
+
+  let(:ticket) do
+    project.tickets.create(:title => "State transitions",
+                           :description => "Can't be hacked.")
+  end
+
+  let(:state) { State.create!(:name => "New") }
+  let(:user) { create_user! }
+
+  describe "language is English" do
+    before(:each) do
+      I18n.locale = "en"
+      I18n.default_locale = "en"
+    end
+
+    context "a user without permission to set state" do
+      before do
+        sign_in(:user, user)
+      end
+
+      it "cannot transition a state by passing through state_id" do
+        post :create, { :comment => { :text => "Hacked!", :state_id => state.id },
+                        :ticket_id => ticket.id }
+        ticket.reload
+        ticket.state.should eql(nil)
+      end
+    end
+  end
+
+
+  describe "選擇語言為中文(zh-HK)" do
+    before(:each) do
+      I18n.locale = "zh-HK"
+      I18n.default_locale = "zh-HK"
+    end
+
+    context "用戶未有更新工作單狀態權限" do
+      before do
+        sign_in(:user, user)
+      end
+
+      it "不能因加入回應而改變工作單狀態" do
+        post :create, { :comment => { :text => "Hacked!", :state_id => state.id },
+                        :ticket_id => ticket.id }
+        ticket.reload
+        ticket.state.should eql(nil)
+      end
+    end
+  end
 end
